cc1fade8688772372f636d12c7f8606d70a75486
zs <zs@qq.com>
2021-11-28 13:09:16 +0800

    hot-fix commit1

--- src/main/java/atguigu/git/GitTest.java
+++ src/main/java/atguigu/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("hello emmm2");
-        System.out.println("hello emmm3");
+        System.out.println("hello emmm hot fix");
     }
 }
