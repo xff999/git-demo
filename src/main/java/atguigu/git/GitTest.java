b7028c2225e5545622b8ca2d5055b7374fbcc70b
zs <zs@qq.com>
2021-11-28 13:14:43 +0800

    master commit3

--- src/main/java/atguigu/git/GitTest.java
+++ src/main/java/atguigu/git/GitTest.java
@@ -4,7 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("hello emmm2");
-        System.out.println("hello emmm master");
+        System.out.println("hello emmm3");
+        System.out.println("hello emmm 3");
         System.out.println("hello emmm hot fix");
         System.out.println("hello emmm hot fix");
     }
